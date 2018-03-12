263d638ff9f9ff7c0f08fafc6544a7ed10befecc
Bart Ledoux <ledouxb@gmail.com>
2018-03-12 00:24:55 -0500

    add sfc Splitter test file

---
+++ __tests__/sfcSplitter.spec.js
@@ -0,0 +1,10 @@
+describe('sfcSplitter', () => {
+    it('should extract the template', async () => {
+    })
+    
+    it('should extract the script', async () => {
+    })
+    
+    it('should extract the style', async () => {
+    })
+})
