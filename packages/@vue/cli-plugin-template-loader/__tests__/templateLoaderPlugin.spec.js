ac475b3e710a7f46c1c548f5b9f9e9bbbd54a51c
Bart Ledoux <ledouxb@gmail.com>
2018-03-20 21:55:14 -0500

    fix(tests): use lerna to make project work testably

--- packages/@vue/cli-plugin-template-loader/__tests__/templateLoaderPlugin.spec.js
+++ packages/@vue/cli-plugin-template-loader/__tests__/templateLoaderPlugin.spec.js
@@ -2,25 +2,26 @@ jest.setTimeout(20000)
 
 const create = require('@vue/cli-test-utils/createTestProject')
 const path = require('path')
+const cwd = path.resolve(__dirname, '../../../test')
 
-test('mocha no sfc', async () => {
-  const project = await create('ts-unit-mocha-no-sfc', {
+test('mocha', async () => {
+  const project = await create('ts-unit-mocha', {
     plugins: {
       '@vue/cli-plugin-typescript': {},
       '@vue/cli-plugin-unit-mocha': {},
-      'vue-cli-plugin-template-loader': {}
+      '@vue/cli-plugin-template-loader': {}
     }
-  })
+  }, cwd)
   await project.run(`vue-cli-service test`)
 })
 
-test('jest no sfc', async () => {
-  const project = await create('ts-unit-jest-no-sfc', {
+test('jest', async () => {
+  const project = await create('ts-unit-jest', {
     plugins: {
       '@vue/cli-plugin-typescript': {},
       '@vue/cli-plugin-unit-jest': {},
-      'vue-cli-plugin-template-loader': {}
+      '@vue/cli-plugin-template-loader': {}
     }
-  })
+  }, cwd)
   await project.run(`vue-cli-service test`)
 })
