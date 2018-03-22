803e8925bf3717cb544cf4f819c9501237a333b9
Bart Ledoux <ledouxb@gmail.com>
2018-03-21 23:59:46 -0500

    fix(test): Add some tests and update prettierrc with 120

---
+++ packages/src/vue-cli-plugin-template-loader/__tests__/invoke.spec.js
@@ -0,0 +1,32 @@
+jest.setTimeout(20000)
+
+const create = require('@vue/cli-test-utils/createTestProject')
+const path = require('path')
+const cwd = path.resolve(__dirname, '../../../test')
+
+async function createAndInstall (name, options) {
+  const project = await create(
+    name,
+    {
+      plugins: {
+        '@vue/cli-plugin-typescript': options
+      }
+    },
+    cwd
+  )
+  // mock install
+  const pkg = JSON.parse(await project.read('package.json'))
+  pkg.devDependencies['vue-cli-plugin-template-loader'] = '*'
+  await project.write('package.json', JSON.stringify(pkg, null, 2))
+  return project
+}
+
+test('invoke', async () => {
+  const project = await createAndInstall(`invoke`, {})
+  await project.run(`${require.resolve('@vue/cli/bin/vue')} invoke vue-cli-plugin-template-loader`)
+})
+
+test('invoke with classComponent', async () => {
+  const project = await createAndInstall(`invoke-classComponent`, { classComponent: true })
+  await project.run(`${require.resolve('@vue/cli/bin/vue')} invoke vue-cli-plugin-template-loader`)
+})
